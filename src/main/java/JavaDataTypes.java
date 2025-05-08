513b6836010b496288a082d2b273c7fe56692c21
redbirdfan <redbirdfan38@gmail.com>
2025-05-08 14:37:48 -0500

    JavaDataTypes problem complete

--- src/main/java/JavaDataTypes.java
+++ src/main/java/JavaDataTypes.java
@@ -2,45 +2,47 @@
 }
 https://www.hackerrank.com/challenges/java-datatypes/problem?isFullScreen=true
 5-7-25 Not finished, partially working.
-5-8-25 Output correct, except last input of 10^13 not reading as a long.
+5-7-25 Output correct, except last input of 10^13 not reading as a long.
+5-8-25 Int changed to Long to handle all numbers.  Also able to take string of numbers now.
 
  */
-import java.util.*;
+
 import java.io.*;
+import java.util.*;
 
+public class Solution {
 
-
-class Solution{
-    public static void main(String []argh)
-    {
-
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         Integer T = scanner.nextInt();
 
-        for (int i = 0; i < T; i++){
-            try{
-                Integer n  = scanner.nextInt();
+        for (int i = 0; i < T; i++) {
+            try {
+                long n = scanner.nextLong();
 
                 System.out.println(n + " can be fitted in:");
 
-                if(n >= -128 && n <= 127){
+                if (n >= Byte.MIN_VALUE && n <= Byte.MAX_VALUE) {
                     System.out.println("* byte");
                 }
 
-                if(n >= -32768 && n <= 32767){
+                if (n >= Short.MIN_VALUE && n <= Short.MAX_VALUE) {
                     System.out.println("* short");
                 }
 
-                if(n >= -2147483648 && n <= 2147483647){
+                if (n >= Integer.MIN_VALUE && n <= Integer.MAX_VALUE) {
                     System.out.println("* int");
                 }
 
-                if(n > Math.pow(-2, 63) && n < ((Math.pow(2, 63))-1)){
+                if (n >= Long.MIN_VALUE && n <= Long.MAX_VALUE) {
                     System.out.println("* long");
                 }
-            }  catch (Exception e) {
-                System.out.println(scanner.next() + " can't be fitted anywhere.");
+            } catch (InputMismatchException e) {
+                String input = scanner.next();
+                System.out.println(input + " can't be fitted anywhere.");
             }
         }
+        scanner.close();
     }
 }
