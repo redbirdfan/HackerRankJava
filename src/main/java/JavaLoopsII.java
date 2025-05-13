912510d64c4efcd63072aa37dde73dd0e1aaa104
redbirdfan <redbirdfan38@gmail.com>
2025-05-13 14:34:32 -0500

    Working through logic to all equations

--- src/main/java/JavaLoopsII.java
+++ src/main/java/JavaLoopsII.java
@@ -1,5 +1,33 @@
 public class JavaLoopsII {
     /*problem link https://www.hackerrank.com/challenges/java-loops/problem?isFullScreen=true//
-
+    5-13-25 still working through
 */
+    import java.io.*;
+import java.util.*;
+
+    public class Solution {
+
+        public static void main(String[] args) {
+            /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+            Scanner scanner = new Scanner(System.in);
+
+            Integer z = scanner.nextInt();
+            Integer a = scanner.nextInt();
+            Integer b = scanner.nextInt();
+            Integer n = scanner.nextInt();
+            System.out.println(a);
+
+            for (int i = 0; i < z; i++){
+                Integer query = (a + 1 * b);
+                System.out.println(query);
+                for(int j = 1; j < n ; j++){
+                    Double exponent = Math.pow(2, i);
+                    Double addOnQuery = (exponent * b);
+                    Double next = (query)+(addOnQuery);
+                }
+
+
+            }
+        }
+    }
 }
