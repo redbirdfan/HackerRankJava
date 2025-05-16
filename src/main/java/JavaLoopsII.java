9cc8b5aed075cc3e004d29abaa9b87e5312f45c8
redbirdfan <redbirdfan38@gmail.com>
2025-05-16 15:15:27 -0500

    Loops for working for 2 test cases and not the other 3.  Will have to check if the code is accounting for edge cases that may be occuring with the input values

--- src/main/java/JavaLoopsII.java
+++ src/main/java/JavaLoopsII.java
@@ -1,8 +1,11 @@
 public class JavaLoopsII {
     /*problem link https://www.hackerrank.com/challenges/java-loops/problem?isFullScreen=true//
     5-13-25 still working through
-*/
-    import java.io.*;
+    5-16-25 First 2 test cases are passing.  3, 4, and 5 are not.  Will have to continue to work through.
+     */
+
+
+import java.io.*;
 import java.util.*;
 
     public class Solution {
@@ -11,23 +14,30 @@ import java.util.*;
             /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
             Scanner scanner = new Scanner(System.in);
 
-            Integer z = scanner.nextInt();
-            Integer a = scanner.nextInt();
-            Integer b = scanner.nextInt();
-            Integer n = scanner.nextInt();
-            System.out.println(a);
+            int z = scanner.nextInt();
 
             for (int i = 0; i < z; i++){
-                Integer query = (a + 1 * b);
-                System.out.println(query);
+
+                int a = scanner.nextInt();
+                int b = scanner.nextInt();
+                int n = scanner.nextInt();
+                int power = 0;
+                int total = (int) (a + Math.pow(2, power) * b);
+                System.out.print(total + " ");
+
                 for(int j = 1; j < n ; j++){
-                    Double exponent = Math.pow(2, i);
-                    Double addOnQuery = (exponent * b);
-                    Double next = (query)+(addOnQuery);
+
+                    power = j;
+
+                    int addOn = (int) Math.pow(2, power) * b;
+                    total = (total + addOn);
+                    System.out.print(total+ " ");
+
+                    if(j == n-1){
+                        System.out.println();
+                    }
+
                 }
-
-
             }
         }
     }
-}
