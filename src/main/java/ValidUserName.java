b02379e3266acf2f3faa7a97fea40787d7f1492f
redbirdfan <redbirdfan38@gmail.com>
2025-06-26 20:25:27 -0500

    Almost all functioning, @ symbol slipping things up

--- src/main/java/ValidUserName.java
+++ src/main/java/ValidUserName.java
@@ -1,7 +1,9 @@
 public class ValidUserName {
     //first run, check length and 1st number.  Now need to complete check against illegal characters.
-    import java.io.*;
-    import java.util.*;
+    //reading as should for valid and invalid.  @ system in an username seems to be tripping it up.
+
+import java.io.*;
+import java.util.*;
 
     public class Solution {
 
@@ -16,18 +18,32 @@ public class ValidUserName {
             String lower = "abcdefghijklmnopqrstuvwxyz";
             String upper = lower.toUpperCase();
             String notLetters = "0123456789_";
+            String firstLetterAcceptable = lower + upper;
             String allAcceptable = (lower+upper+notLetters);
 
             for(int i = 0; i < numNames; i++){
                 String name = scanner.next();
                 char firstLetter = name.charAt(0);
                 String firstChar = Character.toString(firstLetter);
-                if(notLetters.contains(firstChar) || name.length() < 8 || name.length() > 30){
+
+                for(char c : name.toCharArray()){
+                    String c2 = Character.toString(c);
+                    if(!allAcceptable.contains(c2)){
+                        System.out.println("Invalid");
+                        break;
+                    }
+                }
+
+                if(name.length() < 8 || name.length() > 30){
                     System.out.println("Invalid");
+                } else if(!firstLetterAcceptable.contains(firstChar)){
+                    System.out.println("Invalid");
+                } else {
+                    System.out.println("Valid");
                 }
 
             }
+
+
         }
     }
-}
-}
